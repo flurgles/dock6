1e7d4e04fadbe8baec8d01705ea8d3f1c140bbc4
John Bickel <Garn0123@gmail.com>
2026-04-02 15:00:43 -0400

    DOCK6.10 initial release

---
+++ src/dock/version.h
@@ -0,0 +1,10 @@
+// definition of the DOCK version.
+
+#ifndef VERSION_H
+#define VERSION_H
+
+const char *const DOCK_VERSION="DOCK v6.10" ;
+
+const char *const DOCK_RELEASE_DATE="January 2023" ;
+
+#endif  // VERSION_H
